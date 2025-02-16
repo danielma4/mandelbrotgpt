01b24ded77f19610d490f694258c987dc19d91b0
danielma4 <ma.daniel004@gmail.com>
2025-02-16 14:13:01 -0500

    trying my best

--- MathEQGPT/settings.py
+++ MathEQGPT/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 from decouple import config
 import os
 
-PORT = os.environ.get("PORT", 8000)  # Default to 8000 locally
+PORT = os.getenv("PORT", "8000")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
